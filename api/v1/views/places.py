0204a7a2120f24b4dacc8fc07207ebbae60105cb
eyasu chaka <daa282386@gmail.com>
2023-04-29 02:56:04 -0700

    init_place_api

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,29 +2,28 @@
 """create api for PLACE same as State"""
 
 from flask import jsonify, abort, make_response, request
-import sys
-sys.path.insert(1, "/tmp_api/AirBnB_clone_v3")
+# import sys
+# sys.path.insert(1, "/tmp_api/AirBnB_clone_v3")
 from models import storage
 from models.city import City
 from models.place import Place
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places/',\
-    methods=['GET'], strict_slashes=False)
+@app_views.route(
+    '/cities/<city_id>/places/', methods=['GET'], strict_slashes=False)
 def place_by_city(city_id):
     """list all places"""
     city = storage.get("City", city_id)
     if city is not None:
-        _list_of_city = [] 
+        _list_of_city = []
         for place in city.places:
             _list_of_city.append(place.to_dict())
         return jsonify(_list_of_city)
     abort(404)
 
 
-@app_views.route('/places/<place_id>/', methods=['GET'],\
-    strict_slashes=False)
+@app_views.route('/places/<place_id>/', methods=['GET'], strict_slashes=False)
 def get_place(place_id=None):
     """retrieve place by id"""
     place = storage.get("Place", place_id)
@@ -34,16 +33,8 @@ def get_place(place_id=None):
     abort(404)
 
 
-@app_views.route('/places/', methods=['GET'], strict_slashes=False)
-def all_tmp_places():
-    """retrieve all state object"""
-    _list_of_dict = []
-    for city in list(storage.all("Place").values()):
-        _list_of_dict.append(city.to_dict())
-    return jsonify(_list_of_dict)
-
-
-@app_views.route('/cities/<city_id>/places/', methods=['POST'], strict_slashes=False)
+@app_views.route(
+    '/cities/<city_id>/places/', methods=['POST'], strict_slashes=False)
 def post_place(city_id):
     """post new state"""
     if not request.get_json():
@@ -57,13 +48,12 @@ def post_place(city_id):
     user_id = kwargs['user_id']
     if not storage.get("User", user_id):
         abort(404)
-    new_place = Place(**kwargs) # request.get_json())
+    new_place = Place(**kwargs)  # request.get_json())
     new_place.save()
     return make_response(jsonify(new_place.to_dict()), 201)
 
 
-@app_views.route('/places/<place_id>/',\
-    methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>/', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """if place_id is not None find and update place"""
     if not request.get_json():
@@ -71,7 +61,8 @@ def update_place(place_id):
     update_place = storage.get("Place", place_id)
     if update_place is not None:
         for key, value in request.get_json().items():
-            if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+            if key not in \
+                    ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
                 setattr(update_city, key, value)
         update_city.save()
         return make_response(jsonify(update_city.to_dict()), 200)
@@ -79,8 +70,8 @@ def update_place(place_id):
         abort(404)
 
 
-@app_views.route('/places/<place_id>/',\
-    methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/places/<place_id>/', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """if place_id is not None find and \
         delete place_info"""
