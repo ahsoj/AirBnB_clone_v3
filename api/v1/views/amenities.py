e966ffd007211a0f078b2d3c57b04af04a6b20c9
eyasu chaka <daa282386@gmail.com>
2023-04-29 02:43:28 -0700

    init_amenity_api

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """retrieve amenity objects to api"""
-import sys
+
+# import sys
 from flask import jsonify, make_response, abort, request
-sys.path.insert(1, "/tmp_api/AirBnB_clone_v3")
+# sys.path.insert(1, "/tmp_api/AirBnB_clone_v3")
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -13,14 +14,12 @@ def all_amenities():
     """retrieve all amenity object"""
     _list_of_dict = []
     for state in list(storage.all("Amenity").values()):
-        # print(state.to_dict())
         _list_of_dict.append(state.to_dict())
-    # print([_list_of_dict])
     return jsonify(_list_of_dict)
 
 
-@app_views.route('/amenities/<amenity_id>',\
-   methods=['GET'], strict_slashes=False)
+@app_views.route(
+    '/amenities/<amenity_id>/', methods=['GET'], strict_slashes=False)
 def single_amenity(amenity_id):
     """if amenity_id not None return singel amenity_info"""
     amenity_info = storage.get("Amenity", amenity_id)
@@ -41,8 +40,8 @@ def post_amenity():
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>',\
-    methods=['PUT'], strict_slashes=False)
+@app_views.route(
+    '/amenities/<amenity_id>/', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """if user_id is not None find and update amenity"""
     if not request.get_json():
@@ -56,11 +55,10 @@ def update_amenity(amenity_id):
         return make_response(jsonify(update_amenity.to_dict()), 200)
     else:
         abort(404)
-    
 
 
-@app_views.route('/amenities/<amenity_id>',\
-    methods=['DELETE'], strict_slashes=False)
+@app_views.route(
+    '/amenities/<amenity_id>/', methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """if amenity_id is not None find and \
         delete amenity_info"""
